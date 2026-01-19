40d744c44161a1c2a5fc82d431582079a3d52d89
Harsh <cloudopsengineerharsh@gmail.com>
2026-01-19 22:47:25 +0530

    Added Jenkinsfile and Terraform files

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+
+variable "ami" {
+   type        = string
+   description = "Ubuntu AMI ID"
+   default     = "ami-0ecb62995f68bb549"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "My EC2 Instance"
+}
